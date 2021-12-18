e07f3a8a36b14f3ad4d2899bea9984dfa43767e7
eb <eric.boniardi@mail.polimi.it>
2021-12-18 11:28:20 +0100

    multiple java intance avoided

--- root/functions/correct_text.py
+++ root/functions/correct_text.py
@@ -1,11 +1,8 @@
-import language_tool_python
 
 
-def correct_text(transcription):
+def correct_text(transcription, tool):
     fun_name = 'correct_text'
     print(fun_name + ' started')
-
-    tool = language_tool_python.LanguageTool('en-US')
 
     print(fun_name + ' finished')
     return tool.correct(transcription)
--- root/main.py
+++ root/main.py
@@ -1,3 +1,4 @@
+import language_tool_python
 from IPython.display import Audio
 from functions.correct_text import correct_text
 from functions.find_prove_it.find_prove_it import find_prove_it
@@ -7,13 +8,16 @@ from functions.tokenize_video.tokenize_video import tokenize_video
 from functions.tokenize_video.video_token import VideoToken
 
 if __name__ == '__main__':
-    folder = '/home/eb/Videos/arf/'
+    #folder = '/home/eb/Videos/arf/'
+    folder = '/Users/eb/Movies/arf/'
     file_input = 'input.mp4'
     file_output_temp = 'temp_output.mp4'
     to_find = 'set'
     secs = 30 ## secs
 
     video_tokens: [VideoToken] = tokenize_video(folder + file_input, secs)
+
+    tool = language_tool_python.LanguageTool('en-US')
 
     found_log = ''
     final_text = ''
@@ -24,7 +28,7 @@ if __name__ == '__main__':
 
         transcription = generate_transcription(output_audio_f)
 
-        transcription_updated = correct_text(transcription)
+        transcription_updated = correct_text(transcription, tool)
         #print(transcription_updated)
         final_text += transcription_updated
 
